1910d2e17ec31549f77f149b0df1c6a96e0e3490
Aecio Cleysson <aeciocleysson@hotmail.com>
2023-03-31 18:11:30 -0300

    Alteração e migration 1.0.6

--- AntaresSystemWeb/Migrations/AntaresDb/20230331185612_v1.0.6.Designer.cs
+++ AntaresSystemWeb/Migrations/AntaresDb/20230331185612_v1.0.6.Designer.cs
@@ -4,6 +4,7 @@ using AntaresSystemWeb.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AntaresSystemWeb.Migrations.AntaresDb
 {
     [DbContext(typeof(AntaresDbContext))]
-    partial class AntaresDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230331185612_v1.0.6")]
+    partial class v106
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AntaresSystemWeb/Migrations/AntaresDb/20230331185612_v1.0.6.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AntaresSystemWeb.Migrations.AntaresDb
+{
+    public partial class v106 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LogPonto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Matricula = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StatusEvento = table.Column<int>(type: "int", nullable: false),
+                    FuncionarioId = table.Column<int>(type: "int", nullable: false),
+                    Inserted = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsAtivo = table.Column<bool>(type: "bit", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    UserInserted = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserAlteration = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Deleted = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LogPonto", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LogPonto_Funcionario_FuncionarioId",
+                        column: x => x.FuncionarioId,
+                        principalTable: "Funcionario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LogPonto_FuncionarioId",
+                table: "LogPonto",
+                column: "FuncionarioId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LogPonto");
+        }
+    }
+}
