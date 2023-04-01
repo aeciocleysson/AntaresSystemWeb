e41791395a0516247343f469b93dd66d25bd06a8
Aecio Cleysson <aeciocleysson@hotmail.com>
2023-04-01 00:46:43 -0300

    Migration v1.0.7

---
+++ AntaresSystemWeb/Migrations/AntaresDb/20230401033837_v1.0.7.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AntaresSystemWeb.Migrations.AntaresDb
+{
+    public partial class v107 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
